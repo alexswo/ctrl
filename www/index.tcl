1316e05be118dacfd2ffed81105d3d0d5bcd643c
Alex Oh <alexswo@ucla.edu>
2017-07-13 11:15:28 -0700

    Checkboxes now work'

--- www/index.tcl
+++ www/index.tcl
@@ -1,16 +1,15 @@
 set user_id [ad_conn user_id]
-
-set events_options [ctrl_ce::get_events -calendar_id "ctrlcalendar%40gmail.com"]
+set events_options [ctrl_ce::get_events_cache -calendar_id "ctrlcalendar%40gmail.com"]
 set selected_events [ctrl_ce::get_user_events -user_id $user_id]
-
+ns_log notice "These are the events options: $events_options"
+ns_log notice "These are the selected events: $selected_events"
 
 ad_form -name calendar -export {user_id return_url} -form {
-    {events:integer(checkbox),multiple,optional
+    {events:string(checkbox),multiple,optional
 	{options $events_options}
 	{values $selected_events}
     }
 } -html {
 } -on_submit {
     ctrl_ce::modify_user_events -user_id $user_id -selected_events $selected_events -events $events
-} -after_submit {
 }
